1ab5bcb3c7498d571e05baa9e0823323c18b8b9c
Tsairus Beasley <114105291+EYEProphet@users.noreply.github.com>
2025-01-10 10:40:59 -0500

    Delete .synopsys_dve_rebuild.tcl

--- .synopsys_dve_rebuild.tcl
+++
@@ -1,9 +0,0 @@
-# DVE Simulation Rebuild/Restart Options
-# Saved on Thu Mar 28 22:16:32 2024
-set SIMSETUP::REBUILDOPTION 1
-set SIMSETUP::REBUILDCMD {vcs -sverilog -debug ~/private/240/library.sv zorgGame.sv}
-set SIMSETUP::REBUILDDIR {}
-set SIMSETUP::RESTOREBP 1
-set SIMSETUP::RESTOREDUMP 1
-set SIMSETUP::RESTOREFORCE 1
-set SIMSETUP::RESTORESPECMAN 0
